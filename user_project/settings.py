41146445551167a06e6f1807e3878dcda4013e6c
T1g <ericjchristianson16@gmail.com>
2024-07-16 14:39:37 -0400

    setting it back to allowed origins list

--- user_project/settings.py
+++ user_project/settings.py
@@ -54,13 +54,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# CORS_ALLOWED_ORIGINS = [
-#     'http://localhost:3000/*',
-#     'http://dungeon-meshi-api.cloud.s3-website.us-east-2.amazonaws.com/*',
-#     'http://18.118.89.11/*'
-# ]
-
-CORS_ALLOW_ALL_ORIGINS: True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000/',
+    'http://dungeon-meshi-api.cloud.s3-website.us-east-2.amazonaws.com/',
+    'http://18.118.89.11/'
+]
 
 ROOT_URLCONF = 'user_project.urls'
 
