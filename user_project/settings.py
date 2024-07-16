c6ae074bb997bbaf1eafffa0c540cb46ce468969
T1g <ericjchristianson16@gmail.com>
2024-07-16 14:31:52 -0400

    rearranging settings

--- user_project/settings.py
+++ user_project/settings.py
@@ -37,10 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user_app',
     'rest_framework',
-    'gunicorn',
     'corsheaders',
+    'gunicorn',
+    'user_app',
 ]
 
 MIDDLEWARE = [
