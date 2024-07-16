fb72f35f2fecd71b3eca607c21a7333abb5557d4
T1g <ericjchristianson16@gmail.com>
2024-07-16 14:23:36 -0400

    trying to allow all origins

--- user_project/settings.py
+++ user_project/settings.py
@@ -55,11 +55,13 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000/*',
-    'http://dungeon-meshi-api.cloud.s3-website.us-east-2.amazonaws.com/*',
-    'http://18.118.89.11/*'
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000/*',
+#     'http://dungeon-meshi-api.cloud.s3-website.us-east-2.amazonaws.com/*',
+#     'http://18.118.89.11/*'
+# ]
+
+CORS_ALLOW_ALL_ORIGINS: True
 
 ROOT_URLCONF = 'user_project.urls'
 
